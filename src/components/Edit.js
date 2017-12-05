1505f7e2fd2135540088576d1914f58ef20c123e
meganmfisher <meganmichellefisher@gmail.com>
2017-12-05 10:37:48 -0700

    edit done

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -33,8 +33,15 @@ class Edit extends Component {
     
     // insert updatePost 
     updatePost() {
-        axios.put(`/api/blog/${this.props.match.params.id}`).then(res => {
+        var body = {
+            title: this.state.title,
+            subTitle: this.state.subTitle,
+            image: this.state.image,
+            text: this.state.text
+        }
 
+        axios.put(`/api/blog/${this.props.match.params.id}`, body).then(res => {
+            this.props.history.push(`/blog/${this.props.match.params.id}`)
         }).catch(console.log)
     }
     
@@ -42,7 +49,7 @@ class Edit extends Component {
     // Insert into the deletePost 
     deletePost() {
         axios.delete(`/api/blog/${this.props.match.params.id}`).then(res => {
-
+            this.props.history.push('/search')
         }).catch(console.log)
     }
 
